313c41031c8cf50d88d2dc581f89c49840ac030a
Tanner Dale <tanner@tannerdale.com>
2022-01-11 11:03:15 -0600

    Plant some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Company.destroy_all
+User.destroy_all
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+comp = Company.create(
+  name: 'Bob Ross Inc',
+  password: 'happylittletree',
+  password_digest: 'happylittletree'
+)
+
+User.create(
+  first_name: 'Bob',
+  last_name: 'Ross',
+  email: 'bob_ross@painting.com',
+  password: 'hello_world',
+  password_digest: 'hello_world',
+  role: 2,
+  company_id: comp.id
+)
+
+User.create(
+  first_name: 'Luke',
+  last_name: 'Skywalker',
+  email: 'luke@theforce.com',
+  password: 'iamajedi',
+  password_digest: 'iamajedi',
+  role: 1,
+  company_id: comp.id
+)
+
+User.create(
+  first_name: 'Bobba',
+  last_name: 'Fett',
+  email: 'simple_man@likemyfather.com',
+  password: 'makingwaythroughgalaxy',
+  password_digest: 'makingwaythroughgalaxy',
+  role: 0,
+  company_id: comp.id
+)
