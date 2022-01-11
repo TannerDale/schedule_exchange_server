092f53f562b3f047fd72f49251eb8bd5395b27f0
Tanner Dale <tanner@tannerdale.com>
2022-01-11 11:13:15 -0600

    Add user create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,21 @@
 class Api::V1::UsersController < Api::V1::BaseController
-  def create; end
+  def create
+    @user = User.create!(user_params)
+
+    render json: V1::UserSerializer.new(@user)
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+      :company_id,
+      :role
+    )
+  end
 end
