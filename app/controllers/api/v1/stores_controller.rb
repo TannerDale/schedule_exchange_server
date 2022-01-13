f9d41fe00e6c38a659f835e3f5da95aa435e4fa8
Tanner Dale <tanner@tannerdale.com>
2022-01-12 19:11:07 -0600

    Add stores controller

---
+++ app/controllers/api/v1/stores_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::StoresController < V1::BaseController
+  def index; end
+
+  def show; end
+
+  def create
+    store = Store.create!(store_params)
+    render json: V1::StoreSerializer.new(store), status: 201
+  end
+
+  private
+
+  def store_params
+    params.permit(:street_address, :zip_code, :store_number, :company_id)
+  end
+end
