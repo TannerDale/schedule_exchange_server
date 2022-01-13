7005aa7a4336af72d2b23df33529d209f9ca00ba
Tanner Dale <tanner@tannerdale.com>
2022-01-12 19:10:45 -0600

    Add store routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[show create]
+
+      resources :stores, only: %i[index show create]
     end
   end
 end
